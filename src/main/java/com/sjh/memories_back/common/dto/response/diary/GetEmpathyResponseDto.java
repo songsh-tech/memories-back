6a30c7a133da79f61170ff6e13835efb497b53d2
songsh-tech <hunsiskj16@naver.com>
2025-03-26 17:42:01 +0900

    2025.3.26. 17:42

--- src/main/java/com/sjh/memories_back/common/dto/response/diary/GetEmpathyResponseDto.java
+++ src/main/java/com/sjh/memories_back/common/dto/response/diary/GetEmpathyResponseDto.java
@@ -9,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import com.sjh.memories_back.common.dto.response.ResponseDto;
 import com.sjh.memories_back.common.entity.EmpathyEntity;
 
+import lombok.Getter;
 
+@Getter
 public class GetEmpathyResponseDto extends ResponseDto {
   
   private List<String> empathies;
