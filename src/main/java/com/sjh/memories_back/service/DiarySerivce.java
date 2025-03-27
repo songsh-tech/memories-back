271cd050962237c1141af934d2dd6de303aefd78
songsh-tech <hunsiskj16@naver.com>
2025-03-27 15:05:23 +0900

    2025.03.27. 15:05

--- src/main/java/com/sjh/memories_back/service/DiarySerivce.java
+++ src/main/java/com/sjh/memories_back/service/DiarySerivce.java
@@ -3,8 +3,10 @@ package com.sjh.memories_back.service;
 import org.springframework.http.ResponseEntity;
 
 import com.sjh.memories_back.common.dto.request.diary.PatchDiaryRequestDto;
+import com.sjh.memories_back.common.dto.request.diary.PostCommentRequestDto;
 import com.sjh.memories_back.common.dto.request.diary.PostDiaryRequestDto;
 import com.sjh.memories_back.common.dto.response.ResponseDto;
+import com.sjh.memories_back.common.dto.response.diary.GetCommentResponseDto;
 import com.sjh.memories_back.common.dto.response.diary.GetDiaryResponseDto;
 import com.sjh.memories_back.common.dto.response.diary.GetEmpathyResponseDto;
 import com.sjh.memories_back.common.dto.response.diary.GetMyDiaryResponseDto;
@@ -18,4 +20,7 @@ public interface DiarySerivce {
 
   ResponseEntity<? super GetEmpathyResponseDto> getEmpathy(Integer diaryNumber);
   ResponseEntity<ResponseDto> putEmpathy(Integer diaryNumber, String userId);
+
+  ResponseEntity<? super GetCommentResponseDto> getComment(Integer diaryNumber);
+  ResponseEntity<ResponseDto> postComment(PostCommentRequestDto dto, Integer diaryNumber, String userId);
 }
--- src/main/java/com/sjh/memories_back/service/implement/DiaryServiceImplement.java
+++ src/main/java/com/sjh/memories_back/service/implement/DiaryServiceImplement.java
@@ -8,13 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.sjh.memories_back.common.dto.request.diary.PatchDiaryRequestDto;
+import com.sjh.memories_back.common.dto.request.diary.PostCommentRequestDto;
 import com.sjh.memories_back.common.dto.request.diary.PostDiaryRequestDto;
 import com.sjh.memories_back.common.dto.response.ResponseDto;
+import com.sjh.memories_back.common.dto.response.diary.GetCommentResponseDto;
 import com.sjh.memories_back.common.dto.response.diary.GetDiaryResponseDto;
 import com.sjh.memories_back.common.dto.response.diary.GetEmpathyResponseDto;
 import com.sjh.memories_back.common.dto.response.diary.GetMyDiaryResponseDto;
+import com.sjh.memories_back.common.entity.CommentEntity;
 import com.sjh.memories_back.common.entity.DiaryEntity;
 import com.sjh.memories_back.common.entity.EmpathyEntity;
+import com.sjh.memories_back.repository.CommentRepository;
 import com.sjh.memories_back.repository.DiaryRepository;
 import com.sjh.memories_back.repository.EmpathyRepository;
 import com.sjh.memories_back.service.DiarySerivce;
@@ -27,6 +31,7 @@ public class DiaryServiceImplement implements DiarySerivce {
 
   private final DiaryRepository diaryRepository;
   private final EmpathyRepository empathyRepository;
+  private final CommentRepository commentRepository;
 
   @Override
   public ResponseEntity<ResponseDto> postDiary(PostDiaryRequestDto dto, String userId) {
@@ -118,6 +123,8 @@ public class DiaryServiceImplement implements DiarySerivce {
       boolean isWriter = writerId.equals(userId);
       if (!isWriter) return ResponseDto.noPermission();
 
+      empathyRepository.deleteByDiaryNumber(diaryNumber);
+      commentRepository.deleteByDiaryNumber(diaryNumber);
       diaryRepository.delete(diaryEntity);
 
     } catch (Exception exception) {
@@ -152,6 +159,9 @@ public class DiaryServiceImplement implements DiarySerivce {
     
     try {
 
+      boolean existDiary = diaryRepository.existsByDiaryNumber(diaryNumber);
+      if (!existDiary) return ResponseDto.noExistDiary();
+
       EmpathyEntity empathyEntity = empathyRepository.findByUserIdAndDiaryNumber(userId, diaryNumber);
       if (empathyEntity == null) {
         empathyEntity = new EmpathyEntity(userId, diaryNumber);
@@ -166,6 +176,44 @@ public class DiaryServiceImplement implements DiarySerivce {
     }
 
     return ResponseDto.success(HttpStatus.OK);
+
+  }
+
+  @Override
+  public ResponseEntity<? super GetCommentResponseDto> getComment(Integer diaryNumber) {
+    
+    List<CommentEntity> commentEntities = new ArrayList<>();
+
+    try {
+
+      commentEntities = commentRepository.findByDiaryNumberOrderByWriteDateDesc(diaryNumber);
+      
+    } catch (Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return GetCommentResponseDto.success(commentEntities);
+
+  }
+
+  @Override
+  public ResponseEntity<ResponseDto> postComment(PostCommentRequestDto dto, Integer diaryNumber, String userId) {
+    
+    try {
+
+      boolean existDiary = diaryRepository.existsByDiaryNumber(diaryNumber);
+      if (!existDiary) return ResponseDto.noExistDiary();
+
+      CommentEntity commentEntity = new CommentEntity(dto, diaryNumber, userId);
+      commentRepository.save(commentEntity);
+
+    } catch(Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return ResponseDto.success(HttpStatus.CREATED);
 
   }
   
